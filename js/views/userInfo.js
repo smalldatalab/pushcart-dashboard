abc64cfedffa7e2016dbbbacd1ef260d8d86659a
Sandy Yu <yu.sandy@live.com>
2014-06-02 11:42:20 -0400

    bug fixes and made password a text field for easier dev testing

--- js/views/userInfo.js
+++ js/views/userInfo.js
@@ -5,9 +5,7 @@
   template:  _.template($('#usersInfoTemplate').html()),
   
   render: function() {
-    console.log(this.model.toJSON());
     this.$el.html(this.template({ user: this.model.toJSON() }));
-    console.log(this.$el);
     return this;
   }
 });
