472217fc2f1e5a8c737dc66a63e3d7dad2abc9de
Sandy Yu <yu.sandy@live.com>
2014-06-09 14:12:47 -0400

    item count per category displaying underneath dropdown

--- js/views/itemCount.js
+++ js/views/itemCount.js
@@ -4,15 +4,17 @@ Pushcart.Views.ItemCount = Backbone.View.extend({
   template: _.template($('#purchaseItemCount').html()),
 
   render: function(purchases){
-    console.log("rendering ItemCount!");
-    console.log(purchases);
+    // console.log("rendering ItemCount!");
+    // console.log(purchases);
 
     ps = purchases;
     
-    console.log(ps); 
+    // console.log(ps); 
     var html = this.template({purchases: purchases});
     // console.log(html);
     this.$el.append(html);
     return this;
   }
 });
+
+
